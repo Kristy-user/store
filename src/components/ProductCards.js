dccf37b1b15f6c1e3271f663ba0f04c04d95b7b8
Nikita <nikitakemper555@gmail.com>
2022-12-22 15:56:57 +0300

    feat: add card design

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -1,31 +1,35 @@
 class ProductCards {
   draw(data) {
+    console.log(data);
     const products = data;
     const fragment = document.createDocumentFragment();
     const cardItemTemp = document.querySelector('#cardTemplate');
     if (products.length > 0) {
       products.forEach((item, idx) => {
         const cardClone = cardItemTemp.content.cloneNode(true);
+        // cardClone.querySelector(
+        //   '.card__body-photo'
+        // ).style.backgroundImage = `url(${item.thumbnail})`;
         cardClone.querySelector(
           '.card__body-photo'
-        ).style.backgroundImage = `url(${item.thumbnail})`;
-        cardClone.querySelector('.card__body-title').textContent = item.title;
-        cardClone.querySelector(
-          '.list__item-category'
-        ).textContent = `Category ${item.category}`;
-
+        ).src = item.thumbnail;
+        cardClone.querySelector('.card__body-title').textContent = item.title.length > 15
+        ? item.title.slice(0, 15) + '..' : item.title;
+        // cardClone.querySelector(
+        //   '.list__item-category'
+        // ).textContent = `Category ${item.category}`;
         cardClone.querySelector(
           '.list__item-brand'
-        ).textContent = `Brand: ${item.brand}`;
+        ).textContent = `${item.brand}`;
         cardClone.querySelector(
           '.list__item-price'
-        ).textContent = `Price: ${item.price}`;
+        ).textContent = `${item.price} $`;
         cardClone.querySelector(
           '.list__item-rating'
-        ).textContent = `Rating: ${item.rating}`;
-        cardClone.querySelector(
-          '.list__item-stock'
-        ).textContent = `Stock: ${item.stock} `;
+        ).innerHTML = `Rating: <strong>${item.rating}</strong>`;
+        // cardClone.querySelector(
+        //   '.list__item-stock'
+        // ).textContent = `Stock: ${item.stock} `;
         cardClone.querySelector('.card__buttons-add').id = `${item.id}`;
         cardClone.querySelector(
           '.card__link-details'
